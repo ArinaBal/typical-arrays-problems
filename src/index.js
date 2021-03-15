6b7cf83b2e96bd8e951ec93d92dcf8b6a9fa8492
ArinaBal <ariane.93@mail.ru>
2021-03-15 20:50:23 +0300

    feat: add solutions for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 exports.min = function min (array) {
-  return 0;
+  
+  if(array === undefined || array.length === 0) {
+    return 0
+  }
+   return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+  }
+   return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+  }
+  var total = array.reduce(function (sum, current) {
+    return sum + current;
+  });
+  return total / array.length;
 }
+
